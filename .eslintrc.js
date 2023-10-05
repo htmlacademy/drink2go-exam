b2b0e6f085e12687091d8258d35dc8226ecbe86e
balesniy <balesniy@yandex.ru>
2023-10-05 11:16:33 +0200

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 2023,
+    "sourceType": "module"
+  },
+  "env": {
+    "es2023": true,
+    "browser": true
+  },
+  "extends": "htmlacademy/vanilla",
+  "rules": {
+    // Additional rules...
+  }
+}
